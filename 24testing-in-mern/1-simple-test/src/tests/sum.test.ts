c89147007e5cde481c0c49d6581aead89ed6c929
Ganesh Sahu <ganeshsahu0108@gmail.com>
2024-10-07 19:39:40 +0530

    added some note and additional test

--- 24testing-in-mern/1-simple-test/src/tests/sum.test.ts
+++ 24testing-in-mern/1-simple-test/src/tests/sum.test.ts
@@ -1,7 +1,7 @@
-import {describe, expect, test} from '@jest/globals';
+import {describe, expect, test , it} from '@jest/globals';
 import {sum,multiply} from '../index';
 
-describe('sum module', () => {
+describe('testing sum module', () => {
   test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
@@ -12,15 +12,27 @@ describe('sum module', () => {
 
   // module for sum where all test case of sum wiill be
   // "test" in place of "it" can be used
+  
+  // we can do nested describe i.e 
+
+  // describe('testing sum module', () => {
+  //   describe('testing sum module', () => {
+  //     test-1
+  //     test-2...
+  //   }
+  // }
 });
 
-describe('multiply module', () => {
-  test('multiply : 1 * 2 to equal 2', () => {
-    expect(multiply(1, 2)).toBe(2);
+// another way is
+describe('testing multiply function', () => {
+  it('multiply : 1 * 2 to equal 2', () => {
+    const finalanswer = multiply(1,2);
+    expect(finalanswer).toBe(2);
+    // expect(multiply(1, 2)).toBe(2);
   });
 
-  test('multiply : 10 * 2 to equal 20', () => {
-    expect(multiply(10, 2)).toBe(20);
+  it('multiply : 10 * -2 should return a nagative number :: -20', () => {
+    expect(multiply(10, -2)).toBe(-20);
   });
 
 });
